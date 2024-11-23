b1668642c9b3851b6b00e6e5d54a49d09f6ef2f2
egorg <egorgradov10@gmail.com>
2024-11-23 14:36:17 +0300

    Base game logic

---
+++ code/settings.py
@@ -0,0 +1,9 @@
+import pygame
+from pygame.math import Vector2 as vector
+from sys import exit
+
+WINDOW_WIDTH, WINDOW_HEIGHT = 1280, 720
+TILE_SIZE = 64
+ANIMATION_SPEED = 6
+BATTLE_OUTLINE_WIDTH = 4
+
