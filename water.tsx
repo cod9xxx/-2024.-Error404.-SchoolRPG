6f380564ee7fff8f7d9b0e64fc16fdd6605acd0d
cod9xxx <88170102+cod9xxx@users.noreply.github.com>
2024-11-26 00:33:45 +0300

    Delete water.tsx

--- water.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.11.0" name="water" tilewidth="64" tileheight="64" tilecount="288" columns="24">
- <image source="D:/PycharmProjects/Хакатон 2024. SchoolRPG (Error404)/graphics/tilesets/coast.png" width="1536" height="768"/>
-</tileset>
